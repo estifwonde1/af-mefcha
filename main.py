8994912fad88121d4d62c47505467cdaf73af8f9
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-07-25 16:21:47 +0300

    healthcheck completed

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+app= FastAPI(title="standalone fastapi",version="1.0.0")
+
+@app.get("/health",tags=["Health"])
+def health_check():
+    return{"status":"ok"}
 def main():
     print("Hello from af-meftcha!")
 
